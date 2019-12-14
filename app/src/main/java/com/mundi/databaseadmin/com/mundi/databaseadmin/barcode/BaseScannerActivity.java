7c536fede8b0679602d8da04cde4dc76c7c3e5e2
Chevy Lin <chevylin0802@gmail.com>
2019-12-14 14:39:49 +0800

    update code

---
+++ app/src/main/java/com/mundi/databaseadmin/com/mundi/databaseadmin/barcode/BaseScannerActivity.java
@@ -0,0 +1,30 @@
+package com.mundi.databaseadmin.com.mundi.databaseadmin.barcode;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.mundi.databaseadmin.R;
+
+public class BaseScannerActivity extends AppCompatActivity {
+    public void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if(ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
